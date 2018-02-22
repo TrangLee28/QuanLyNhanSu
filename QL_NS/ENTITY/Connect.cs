1cca245a9aebb02fbccdda0def16e591ada8ce80
TrangLee28 <lelinhtrang28@gmail.com>
2018-02-22 22:18:48 +0700

    .

---
+++ QL_NS/ENTITY/Connect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QL_NS.ENTITY
+{
+   public static class Connect
+    {
+        public static bool isConnect = false;
+        public static SqlConnection myconnect = new SqlConnection(ConnectString.StringConnect);
+    }
+}
---
+++ QL_NS/ENTITY/ConnectString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_NS.ENTITY
+{
+   public static class ConnectString
+    {
+        public static string ServerName = string.Empty;
+        public static string DatabaseName = string.Empty;
+        public static string UserName = string.Empty;
+        public static string Password = string.Empty;
+        public static bool WinAuthentication = false;//true wwindows false aql
+        public static string StringConnect = string.Empty;
+
+        public static void TaoChuoiKetNoi()
+        {
+            string Temp = string.Empty;
+            Temp = "Data Source =" + ServerName + ";";
+            Temp += "Initial Catalog =" + DatabaseName + ";";
+            if (WinAuthentication == true)
+            {
+                Temp += "Integrated security = true";
+            }
+            else
+            {
+                Temp += "Integrated Security = false; User ID = " + UserName + "; Password = " + Password;
+            }
+
+            StringConnect = Temp;
+        }
+    }
+}
