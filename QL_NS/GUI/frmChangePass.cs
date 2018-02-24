fad6e63d22c05d572f85649da13e8705aab2d397
TrangLee28 <lelinhtrang28@gmail.com>
2018-02-24 21:18:50 +0700

    /

---
+++ QL_NS/GUI/frmChangePass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_NS.GUI
+{
+    public partial class frmChangePass : Form
+    {
+        public frmChangePass()
+        {
+            InitializeComponent();
+        }
+    }
+}
